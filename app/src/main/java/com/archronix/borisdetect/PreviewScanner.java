5dbd90443374f06a1fe2dae4a6fa29b175c7491a
Sven Van Asbroeck <svenv@arcx.com>
2016-11-29 09:30:48 -0500

    Fix int-float casting issue.

--- app/src/main/java/com/archronix/borisdetect/PreviewScanner.java
+++ app/src/main/java/com/archronix/borisdetect/PreviewScanner.java
@@ -88,9 +88,9 @@ public final class PreviewScanner {
     private static int convertYUVtoARGB(int y, int u, int v) {
         int r,g,b;
 
-        r = y + (int)1.402f*v;
+        r = y + (int)(1.402f*v);
         g = y - (int)(0.344f*u +0.714f*v);
-        b = y + (int)1.772f*u;
+        b = y + (int)(1.772f*u);
         r = r>255? 255 : r<0 ? 0 : r;
         g = g>255? 255 : g<0 ? 0 : g;
         b = b>255? 255 : b<0 ? 0 : b;
